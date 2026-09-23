9eede0fa7fa806a78eb93182290ff66501c93fc6
rndyyt <1972245397@qq.com>
2026-09-23 14:56:53 +0800

    ci: deploy static site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vite from './vite.config.ts';
 export default defineConfig({
+  site: 'https://rndyt.github.io',
   output: 'static',
   devToolbar: { enabled: false },
   trailingSlash: 'always',
